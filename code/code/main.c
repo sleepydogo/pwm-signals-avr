753aff7f5cae594fff9f87aa7c2667713d9bc0a6
tom <tomaschattmann@gmail.com>
2023-07-26 16:31:23 -0300

    .

--- code/code/main.c
+++ code/code/main.c
@@ -6,19 +6,44 @@
  */ 
 
 #include <avr/io.h>
+#include <>
 
-
-#define PWM_PERIOD	100
-#define PWM_DELTA	25
+#define PWM_PERIOD	10
+#define PWM_DELTA	3
 #define PWM_OFF		PORTB &=~(1<<PORTB0)
-#define PWM_OFF		PORTB |=(1<<PORTB0)
-#define PWM_START	DDR
+#define PWM_ON		PORTB |=(1<<PORTB0)
+#define PWM_START	DDRB |= (1<<PORTB0)
+
+
+/*
+	Se inicializa en -1 para que cuando se ejecute por primera vez el PWM_SOFTWARE_UPDATE
+	se ponga en alto la senial de salida.
+*/
+static uint8_t PWM_position = -1;
+
+void PWM_SOFTWARE_UPDATE(void) {
+	if (++PWM_position >= PWM_PERIOD) {
+		PWM_position = 0;
+		PWM_ON;
+	} else {
+		if (PWM_position < PWM_DELTA) {
+			PWM_ON;
+		}
+		else {
+			PWM_OFF;
+		}
+	}
+}
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	PWM_START;
+	
+	while (1);
+}
+
+
+ISR (TIMER0_COMPA_vect) {
+	PWM_SOFTWARE_UPDATE();
 }
 
